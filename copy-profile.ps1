7c8680f42b1967d08ebd49e33806948d1262ca42
Rich Turner <rich@bitcrazed.com>
2014-11-06 15:07:58 -0800

    Update to include PowerShell profile

--- copy-profile.ps1
+++ copy-profile.ps1
@@ -26,3 +26,18 @@ echo 'Backing-up files:'
         echo "    Copying `"$source`" to `"$dest`""
         copy -force $source $dest
     }
+
+echo 'Backing-up PowerShell Profile:'
+'Documents\WindowsPowerShell' |
+    % { 
+        $source = "$env:userprofile\$_\*.*"
+        $dest = "$backupPath$_\"
+        
+        if (!(test-path $dest)) { 
+            echo "    Creating path for `"$dest`":"
+            mkdir -force $dest  | out-null
+        }
+
+        echo "    Copying `"$source`" to `"$dest`":"
+        copy $source $dest
+    }
